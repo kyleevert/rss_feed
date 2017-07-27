4db99c578158279af43d32473f26c0ff1212358b
calemyers723 <cale.myers723@gmail.com>
2017-07-27 15:48:37 +0800

    add gon gem, diplay one feed url

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,14 @@ require 'open-uri'
 
 class HomeController < ApplicationController
   def index
-    url = 'https://www.reddit.com/r/Bitcoin/top.rss?sort=top&t=day.rss'
+    url = 'https://www.reddit.com/r/CryptoCurrency/top.rss?sort=top&t=day.rss'
     xml_doc = Nokogiri::XML(open(url, 'User-Agent' => 'Nooby'))
-    xml_contents = xml_doc.xpath("//xmlns:content")
-    xml_contents.each do |aml_content|
-      
+    xml_entries = xml_doc.xpath("//xmlns:entry")
+    @links = []
+    xml_entries.each do |xml_entry|
+      @links << xml_entry.children[4].attributes["href"].value
     end
-    binding.pry
+
+    # @links = Feed.feed_links
   end
 end
