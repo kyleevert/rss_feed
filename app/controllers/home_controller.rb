2d0e14c1e5d1bd212960c5998a44177d6223eba3
calemyers723 <cale.myers723@gmail.com>
2017-07-28 17:39:36 +0800

    reddit links

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,14 +2,6 @@ require 'open-uri'
 
 class HomeController < ApplicationController
   def index
-    # url = 'https://www.reddit.com/r/CryptoCurrency/top.rss?sort=top&t=day.rss'
-    # xml_doc = Nokogiri::XML(open(url, 'User-Agent' => 'Nooby'))
-    # xml_entries = xml_doc.xpath("//xmlns:entry")
-    # @links = []
-    # xml_entries.each do |xml_entry|
-    #   @links << xml_entry.children[4].attributes["href"].value
-    # end
-
     @reddit_links = Feed.reddit_feed_links
     # @google_feed_links = Feed.google_alert_feed_links
   end
