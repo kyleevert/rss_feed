1cd484d0d4968664ef01e24c3b4ea6cc07ba3500
calemyers723 <cale.myers723@gmail.com>
2017-07-28 19:00:29 +0800

    parse feed link from reddit rss feeds

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'rails_12factor', group: [:production, :staging]
 gem 'gon', '~> 6.1'
 gem 'pg', '~> 0.18.0'
+gem 'simple-rss', '~> 1.3', '>= 1.3.1'
+gem 'httparty', '~> 0.13.7'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,18 +1,19 @@
 require 'open-uri'
+require 'httparty'
 
 class Feed
   FEED_URLS = [
     'https://www.reddit.com/r/Bitcoin/top.rss?sort=top&t=day.rss',
-    'https://www.reddit.com/r/CryptoCurrency/top.rss?sort=top&t=day.rss',
-    'https://www.reddit.com/r/ethereum/top.rss?sort=top&t=day.rss',
-    'https://www.reddit.com/r/EthereumClassic/top.rss?sort=top&t=day.rss',
-    'https://www.reddit.com/r/litecoin/top.rss?sort=top&t=day.rss',
-    'https://www.reddit.com/r/Ripple/top.rss?sort=top&t=day.rss',
-    'https://www.reddit.com/r/dashpay/top.rss?sort=top&t=day.rss',
-    'https://www.reddit.com/r/nem/top.rss?sort=top&t=day.rss',
-    'https://www.reddit.com/r/Iota/top.rss?sort=top&t=day.rss',
-    'https://www.reddit.com/r/Monero/top.rss?sort=top&t=day.rss',
-    'https://www.reddit.com/r/stratisplatform/top.rss?sort=top&t=day.rss'
+    # 'https://www.reddit.com/r/CryptoCurrency/top.rss?sort=top&t=day.rss',
+    # 'https://www.reddit.com/r/ethereum/top.rss?sort=top&t=day.rss',
+    # 'https://www.reddit.com/r/EthereumClassic/top.rss?sort=top&t=day.rss',
+    # 'https://www.reddit.com/r/litecoin/top.rss?sort=top&t=day.rss',
+    # 'https://www.reddit.com/r/Ripple/top.rss?sort=top&t=day.rss',
+    # 'https://www.reddit.com/r/dashpay/top.rss?sort=top&t=day.rss',
+    # 'https://www.reddit.com/r/nem/top.rss?sort=top&t=day.rss',
+    # 'https://www.reddit.com/r/Iota/top.rss?sort=top&t=day.rss',
+    # 'https://www.reddit.com/r/Monero/top.rss?sort=top&t=day.rss',
+    # 'https://www.reddit.com/r/stratisplatform/top.rss?sort=top&t=day.rss'
   ]
 
   GOOGLE_FEED_URLS = [
@@ -26,7 +27,12 @@ class Feed
       xml_entries = xml_doc.xpath("//xmlns:entry")
 
       xml_entries.each do |xml_entry|
-        feed_links << xml_entry.children[4].attributes["href"].value
+        # feed_links << xml_entry.children[2].text
+
+        feed_text = xml_entry.children[2].text
+        pos_link = feed_text.index('[link]')
+        pos_href = feed_text.rindex('href=', pos_link)
+        feed_links << feed_text[pos_href + 6..pos_link - 3]
       end
     end
     feed_links
@@ -45,5 +51,15 @@ class Feed
       end
     end
     feed_links
+  end
+
+  def self.simple_rss_feed
+    feed_url = 'https://www.reddit.com/r/Bitcoin/top.rss?sort=top&t=day.rss'
+    xml_doc = Nokogiri::XML(open(feed_url, 'User-Agent' => 'Nooby'))
+    xml_entries = xml_doc.xpath("//xmlns:entry")
+    feed_text = xml_entries.first.children[2].text
+    pos_link = feed_text.index('[link]')
+    pos_href = feed_text.rindex('href=', pos_link)
+    puts feed_text[pos_href + 6..pos_link - 3]
   end
 end
