057f6884e7b1a7e5072164195c9e4fbe963ff0c9
calemyers723 <cale.myers723@gmail.com>
2017-08-10 01:07:20 +0800

    feed parse update

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,9 +1,13 @@
 class FeedController < ApplicationController
   def index
-    feed_urls = params[:reddit_input_rss] || []
+    # feed_urls = params[:reddit_input_rss] || []
+    feed_url_ids_str = params[:reddit_input_rss] || ''
+    feed_urls = InputFeed.where(id: feed_url_ids_str.split(','), feed_type: 'reddit').pluck(:url)
     @reddit_entries = Feed.reddit_feed_entries feed_urls
 
-    feed_urls = params[:google_input_rss] || []
+    # feed_urls = params[:google_input_rss] || []
+    feed_url_ids_str = params[:google_input_rss] || ''
+    feed_urls = InputFeed.where(id: feed_url_ids_str.split(','), feed_type: 'google').pluck(:url)
     @google_entries = Feed.google_feed_entries feed_urls
     respond_to do |format|
       format.rss { render :layout => false }
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,11 +7,11 @@ class HomeController < ApplicationController
   end
 
   def combine_feed
-    reddit_input_rss_feeds = params[:reddit_input_rss]
+    reddit_input_rss_feeds = (params[:reddit_input_rss] || []).uniq
     reddit_input_rss_feeds.delete_if {|x| x.blank? }
-    google_input_rss_feeds = params[:google_input_rss]
+    google_input_rss_feeds = (params[:google_input_rss] || []).uniq
     google_input_rss_feeds.delete_if {|x| x.blank? }
-    @feed_url = feed_url(format: :rss, reddit_input_rss: reddit_input_rss_feeds, google_input_rss: google_input_rss_feeds)
+    @feed_url = feed_url(format: :rss, reddit_input_rss: reddit_input_rss_feeds.join(','), google_input_rss: google_input_rss_feeds.join(','))
     respond_to do |format|
       format.js {
         render :combine_feed
